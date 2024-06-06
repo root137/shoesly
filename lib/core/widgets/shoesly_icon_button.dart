93e45d152dd5435f309c864315791a9f614a310c
root137 <surenyogi393@gmail.com>
2024-06-06 14:43:55 +0545

    theme setup and components build

---
+++ lib/core/widgets/shoesly_icon_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shoesly/core/themes/app_colors.dart';
+import 'package:vector_graphics/vector_graphics_compat.dart';
+
+class ShoeslyIconButton extends StatelessWidget {
+  final String assetImagePath;
+  final double? size;
+  final Color? iconColor;
+  final BoxFit? fit;
+  final VoidCallback? onPressed;
+
+  const ShoeslyIconButton({
+    super.key,
+    required this.assetImagePath,
+    this.size = 24,
+    this.iconColor,
+    this.onPressed,
+    this.fit = BoxFit.contain,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: SvgPicture(
+        AssetBytesLoader(
+          assetImagePath,
+        ),
+        width: size,
+        height: size,
+        colorFilter: ColorFilter.mode(
+          iconColor ?? COLOR_PRIMARY,
+          BlendMode.srcIn,
+        ),
+        fit: fit ?? BoxFit.contain,
+      ),
+    );
+  }
+}
